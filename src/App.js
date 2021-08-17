2cbcb9face71083973113652aa2378ad3992e01c
Prizrak_11 <santipato2004@gmail.com>
2021-08-17 15:18:31 -0500

    Added react-hot-toast for notifications

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Toaster } from 'react-hot-toast'
+
 import './style/normalize.scss'
 import './style/Global.scss';
 import './style/_animations.scss'
+
 import Dashboard from './components/Dashboard/Dashboard'
-//import NavBar from './components/Common/NavBar/NavBar';
 import ErrorPage from './components/ErrorPage/ErrorPage';
-//import Footer from './components/Footer/Footer'
 import PrivateRoute from './router/PrivateRoute'
-import { UserContextProvider } from './context/UserContext/UserContext';
 import LogIn from './components/LogIn/LogIn';
+
+import { UserContextProvider } from './context/UserContext/UserContext';
 
 function App() {
   return (
-    <UserContextProvider>
-      <Router>
-        <Switch>
-          <Route exact path='/login' component={LogIn} />
-          <PrivateRoute exact path='/' component={Dashboard} />
-          <PrivateRoute exact path='/dashboard' component={Dashboard} />
-          <PrivateRoute exact path='/profile' component={() => <h1>Hey</h1>} />
-          <PrivateRoute exact path='/reservations' component={() => <h1>Hey</h1>} />
-          <PrivateRoute exact path='/config' component={() => <h1>Hey</h1>} />
-          <Route path='*' component={ErrorPage} />
-        </Switch>
-      </Router>
-    </UserContextProvider>
+    <>
+      <Toaster />
+      <UserContextProvider>
+        <Router>
+          <Switch>
+            <Route exact path='/login' component={LogIn} />
+            <PrivateRoute exact path='/' component={Dashboard} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            <PrivateRoute exact path='/profile' component={() => <h1>Hey</h1>} />
+            <PrivateRoute exact path='/reservations' component={() => <h1>Hey</h1>} />
+            <PrivateRoute exact path='/config' component={() => <h1>Hey</h1>} />
+            <Route path='*' component={ErrorPage} />
+          </Switch>
+        </Router>
+      </UserContextProvider>
+    </>
   )
 }
 
