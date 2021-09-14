55eee6e7bb0e412e62bb81e3273e56e8cf8ff782
Prizrak_11 <santipato2004@gmail.com>
2021-09-13 21:51:01 -0500

    Add categories to restaurants
    + Added readonly for not main restaurants
    + Add categories to the restaurants

--- src/context/types.js
+++ src/context/types.js
@@ -4,13 +4,16 @@ const TYPES = {
     LOGOUT: 'Log out',
     BRANCHES: 'Save the branches info',
     FRANCHISE: 'Is the user a franchise?',
-    UPDATE_BRANCHES: 'Update all the branches with specific information'
+    UPDATE_BRANCHES: 'Update all the branches with specific information',
+    CATEGORIES: {
+      DISH_C: 'Update the categories of the dishes',
+      RESTAURANT_C: 'Update the categories of the restaurants'
+    }
   },
   MENU: {
     CREATE: {
       DISHESCOPY: 'Create a copy of all dishes in memory',
       DISHSTATUSLIST: 'Create a copy of the statuses of the dishes',
-      CATEGORIES: 'Update the categories of the dishes',
       DISH: 'Create a dish an add it to the database'
     },
     UPDATE: {
